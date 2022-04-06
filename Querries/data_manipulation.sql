2d262bc68dfca13324379145ef6ec9ea4c6bdbd7
austinDL <austin.r.diggins@gmail.com>
2022-04-06 11:05:06 -0400

    Modified the weight log table

--- Querries/data_manipulation.sql
+++ Querries/data_manipulation.sql
@@ -1,18 +1,5 @@
 USE [Fitness Tracker];
 
--- Modify the weight log data to include how often each user logged their weight and how much weight they lost between each log
-SELECT
-	Id, [Date], WeightPounds, BMI,
-	COUNT(*) OVER(PARTITION BY Id) log_count,
-	-- Calculate how much weight each user has lost on each weight log
-	-- Note that a positive value represents a weight gain
-	WeightPounds - LAG(WeightPounds) OVER(PARTITION BY Id ORDER BY [Date]) AS WeightLossPounds
-FROM weight_log_info
--- Order the weight logs by how frequently the users logged their weight
--- Fewer weight logs makes it harder to notice a trend
-ORDER BY log_count DESC, Id, [Date];
-
-
 -- Reformat the ActivityDate column so that it has the same format as the other datetime columns
 SELECT 
 	* , 
@@ -26,3 +13,81 @@ DROP COLUMN ActivityDate;
 
 -- Query the results to save it to a csv file
 SELECT * FROM daily_activity_temp;
+
+-- =========================================================== --
+-- Alter the weight_log_info table to include more information --
+-- =========================================================== --
+ALTER TABLE weight_log_info
+ADD 
+	Number_of_Logs SMALLINT, -- Add a column for the number of logs each user inputs
+	WeightLoss_pounds FLOAT; -- Add a column for the weight loss of each person in between each weigh-in
+-- Remove irrelevant information
+ALTER TABLE weight_log_info
+DROP COLUMN 
+	WeightKg,        -- Repeated information from WeightPounds
+	Fat,             -- Most users don't enter this information
+	LogId;           -- Doesn't tell us anything about the user's health status
+
+-- Populate the weight loss column
+WITH weight_track AS (
+SELECT
+	-- Id and Date are used to determine where to insert the weight log information
+	Id,
+	[Date],
+	-- Calculate how much weight each user has lost on each weight log
+	---- Note that a positive value represents a weight gain
+	WeightPounds - LAG(WeightPounds) OVER(PARTITION BY Id ORDER BY [Date]) AS WeightLossPounds,
+	-- Calculate how many times each person logged their weight
+	COUNT(*) OVER(PARTITION BY Id) AS Log_num
+FROM weight_log_info
+)
+-- Insert the weight loss information into the weight_log_info table
+UPDATE weight_log_info
+SET 
+	weight_log_info.WeightLoss_pounds = weight_track.WeightLossPounds,
+	weight_log_info.Number_of_Logs = weight_track.Log_num
+FROM weight_log_info
+INNER JOIN weight_track
+ON weight_log_info.Id = weight_track.Id AND weight_log_info.[Date] = weight_track.[Date]
+
+-- Check the results
+SELECT * 
+FROM weight_log_info
+ORDER BY
+	Number_of_Logs DESC,
+	Id,
+	[Date];
+
+-- Split the Date from the time for joining this table to other information
+ALTER TABLE weight_log_info
+ADD [Time] TIME;
+UPDATE weight_log_info
+SET 
+	[Time] = FORMAT([Date], 'HH:mm:ss'),
+	[Date] = FORMAT([Date], 'yyyy-MM-dd');
+
+-- Check the results
+SELECT * 
+FROM weight_log_info
+ORDER BY
+	Number_of_Logs DESC,
+	Id,
+	[Date];
+
+/*
+It is of interest to note that there are a significant number of weight_log entries at 23:59:59
+This leads me to believe that this is a missing value.
+Through further inspection, it seems as though the time is 23:59:59 when the user has manually entered their weight,
+which further strengthens the hypothesis that this time represents a missing value
+*/
+UPDATE weight_log_info
+SET [Time] = NULL
+WHERE IsManualReport = 1;
+
+-- Check the results
+SELECT * 
+FROM weight_log_info
+ORDER BY
+	Number_of_Logs DESC,
+	Id,
+	[Date];
